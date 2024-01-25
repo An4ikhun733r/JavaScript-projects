870bee227e1020d72f045d2c325fd8de4b56c026
An4ikhun733r <anuar.ospanov.prof@gmail.com>
2024-01-25 14:29:11 +0600

    .

--- calculator/main.js
+++ calculator/main.js
@@ -1,19 +1,45 @@
-let firstnum, secondnum, operator;
+let fnum = "", snum = "", operator = null;
+let shouldResetScreen = false;
+const numButton = document.querySelectorAll(".inline-button");
+const signButton = document.querySelectorAll(".inline-sign")
+const screen = document.getElementById("display");
+const curscreen = document.getElementById("cur-display")
 
-document.addEventListener("DOMContentLoaded", function() {
-    const numButton = document.querySelectorAll(".inline-button");
-    const screen = document.getElementById("display");
-    numButton.forEach((button) =>
-        button.addEventListener('click', () => appendNumber(button.textContent))
-    )
-    function appendNumber(number) {
-        screen.textContent += number;
+numButton.forEach((button) =>
+    button.addEventListener('click', () => appendNumber(button.textContent))
+)
+
+signButton.forEach((button) =>
+    button.addEventListener('click', () => appendNumber(button.textContent))
+)
+
+function appendNumber(number) {
+    if (curscreen.textContent === '0')
+    {
+        curscreen.textContent = number;
     }
-});
+    else if (word.endsWith("clear")){
+        clear()
+    }
+    else
+    {
+        curscreen.textContent += number;
+    }
+}
 
+function appendSign(sign) {
+    word = screen.textContent;
+    if (sign == '='){
+        let first = word.slice;
+    }
+}
 
-
-
+function clear() {
+    screen.textContent = "0";
+    fnum = 0;
+    snum = 0;
+    operator = null;
+}
 
 function add(a, b){
     return a + b;

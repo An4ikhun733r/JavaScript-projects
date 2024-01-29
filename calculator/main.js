ea07e37fb9bd22864ae67eed1df0248537bc2e9a
Anuar <101927328+An4ikhun733r@users.noreply.github.com>
2024-01-29 21:00:25 +0600

    Update main.js

--- calculator/main.js
+++ calculator/main.js
@@ -2,7 +2,7 @@ let fnum = "";
 let snum = "";
 let prevsign = "";
 let operator = null;
-let shouldResetScreen = false;
+let shouldResetScreen = true;
 const numButton = document.querySelectorAll(".inline-button");
 const signButton = document.querySelectorAll(".inline-sign")
 const lastscreen = document.getElementById("display");
@@ -17,12 +17,23 @@ signButton.forEach((button) =>
 )
 
 function appendNumber(number) {
-    if (curscreen.textContent === '0' || shouldResetScreen) {
+    if (curscreen.textContent === '0') {
         curscreen.textContent = number;
-        shouldResetScreen = false;
+    } else if (number == "="){
+        snum = curscreen.textContent;
+        fnum = parseFloat(fnum);
+        snum = parseFloat(snum);
+        curscreen.textContent = operate(fnum, snum, operator);
+        lastscreen.textContent = "";
     } else {
+        if (isSign(lastscreen.textContent[lastscreen.textContent.length-1]) && shouldResetScreen) {
+            curscreen.textContent = number;
+            shouldResetScreen = false;
+            return
+        }
         curscreen.textContent += number;
     }
+    shouldResetScreen = true;
 }
 
 function isSign(sign){
@@ -38,30 +49,32 @@ function appendSign(sign) {
         return
     }
 
-    if (isSign(sign)){
-        lastscreen.textContent = curscreen.textContent + sign;
-        curscreen.textContent = "";
-    }    
+    if (sign == "delete"){
+        curscreen.textContent = curscreen.textContent.slice(0,-1);
+        return
+    }
 
-    fnum = lastscreen.textContent.slice(0, -1);
+    operator = sign
 
-    if (sign == "="){
+    if (prevsign != operator && prevsign != "") {
+        alert(prevsign)
+        alert(operator)
         snum = curscreen.textContent;
         fnum = parseFloat(fnum);
         snum = parseFloat(snum);
-        alert(fnum)
-        alert(snum)
-        curscreen.textContent = operate(fnum, snum, prevsign);
+        fnum = operate(fnum, snum, prevsign)
+        curscreen.textContent = fnum;
         lastscreen.textContent = "";
-        shouldResetScreen = false;
-    }else{
-        if (isSign(prevsign)){
-            snum = curscreen.textContent;
-            fnum = operate(parseFloat(fnum), parseFloat(snum), sign);
-            curscreen.textContent = fnum + sign;
-        }
-        prevsign = sign;
+        prevsign = ""
     }
+
+    
+    if (isSign(sign)){
+        lastscreen.textContent = curscreen.textContent + sign;
+    }    
+
+    fnum = lastscreen.textContent.slice(0, -1);
+    prevsign = operator
 }
 
 function clear() {
